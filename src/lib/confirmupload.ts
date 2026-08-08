f4c34594d71f665fdea90087caf425ba1111ab6c
Piyush <pk3698855@gmail.com>
2026-08-08 08:19:28 +0530

    feat: add confirmupload func , checks files in r2

---
+++ src/lib/confirmupload.ts
@@ -0,0 +1,36 @@
+import { HeadObjectCommand } from "@aws-sdk/client-s3";
+import { prisma } from "./prisma";
+import { BUCKET_NAME, r2Client } from "./r2";
+
+
+export async function confirmUpload(documentId: string) {
+
+    const documents = await prisma.document.findUnique({
+        where: { id: documentId },
+        select: { id: true, r2Key: true, uploadStatus: true }
+    });
+
+    if (!documents) {
+        return { ok: false as const, reason: "NOT FOUND " as const };
+    }
+    if (documents.uploadStatus == "UPLOADED") {
+        return { ok: true as const, reason: "ALREADY UPLOADED" };
+    }
+
+    try {
+        await r2Client.send(
+            new HeadObjectCommand({ Bucket: BUCKET_NAME, Key: documents.r2Key })
+        )
+    } catch (error) {
+        await prisma.document.update({
+            where: { id: documentId },
+            data: { uploadStatus: "FAILED" }
+        });
+        return { ok: false as const, reason: "FILE NOT IN STORAGE" as const };
+    }
+    const updated = await prisma.document.update({
+        where: { id: documentId },
+        data: { uploadStatus: "UPLOADED" }
+    });
+    return { ok: true as const, status: updated.uploadStatus };
+}
