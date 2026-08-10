e3c9476608a777116b361fd2dbba804b0485a32c
Piyush <pk3698855@gmail.com>
2026-08-10 18:37:17 +0530

    fix: edit r2-endpoint

--- src/lib/r2.ts
+++ src/lib/r2.ts
@@ -2,10 +2,11 @@ import { S3Client } from "@aws-sdk/client-s3";
 
 export const r2Client = new S3Client({
     region: "auto",
+    endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
     credentials: {
         accessKeyId: process.env.R2_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
-    },
-    endpoint: `https://${process.env.R2_ACCOUNT_ID}r2.cloudflarestorage.com`
+        secretAccessKey: process.env.R2_SECRET_ACCESS_KEY_ID!,
+    }
 })
-export const BUCKET_NAME = process.env.R2_BUCKET_NAME;
+export const BUCKET_NAME = process.env.R2_BUCKET_NAME!;
+
