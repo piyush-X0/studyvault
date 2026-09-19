fc2b2ff76756d94fe5fe0978e5899199e60e16b0
Piyush <pk3698855@gmail.com>
2026-09-19 21:21:20 +0530

    Include pdfjs worker

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ["pdf-parse", "pdfjs-dist", "@xenova/transformers", "onnxruntime-node", "sharp", "@napi-rs/canvas"],
   outputFileTracingIncludes: {
     "/*": ["./node_modules/**/onnxruntime-node/bin/napi-v3/linux/x64/*",
-      "./node_modules/@napi-rs/**/*"
+      "./node_modules/@napi-rs/**/*",
+      "./node_modules/pdfjs-dist/legacy/build/**/*"
     ],
   },
   devIndicators: false,
