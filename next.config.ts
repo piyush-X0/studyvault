b9c985e7d607d9e4770b510dd0de50363e136032
Piyush <pk3698855@gmail.com>
2026-09-19 20:51:28 +0530

    Include onnxruntime linux binaries

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['pdf-parse', "@xenova/transformers", 'mammoth'],
+  serverExternalPackages: ["pdf-parse", "@xenova/transformers", "onnxruntime-node", "sharp"],
+  outputFileTracingIncludes: {
+    "/*": ["./node_modules/**/onnxruntime-node/bin/napi-v3/linux/x64/*"],
+  },
   devIndicators: false,
 };
 
