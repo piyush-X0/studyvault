bbbeebbad49c42e797fb35ac17818f0d119e1fbe
Piyush <pk3698855@gmail.com>
2026-09-19 21:05:29 +0530

    Fix pdf-parse canvas on vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["pdf-parse", "@xenova/transformers", "onnxruntime-node", "sharp"],
+  serverExternalPackages: ["pdf-parse", "pdfjs-dist", "@xenova/transformers", "onnxruntime-node", "sharp", "@napi-rs/canvas"],
   outputFileTracingIncludes: {
-    "/*": ["./node_modules/**/onnxruntime-node/bin/napi-v3/linux/x64/*"],
+    "/*": ["./node_modules/**/onnxruntime-node/bin/napi-v3/linux/x64/*",
+      "./node_modules/@napi-rs/**/*"
+    ],
   },
   devIndicators: false,
 };
